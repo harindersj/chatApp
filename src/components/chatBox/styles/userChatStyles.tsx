8001b2f119c3e42029c07ece2bf8c528676ba6cf
harindersj <42744540+harindersj@users.noreply.github.com>
2021-01-17 12:27:00 +0530

    initial commit

---
+++ src/components/chatBox/styles/userChatStyles.tsx
@@ -0,0 +1,37 @@
+
+import { makeStyles } from "@material-ui/core";
+import { FormatAlignJustifyTwoTone } from "@material-ui/icons";
+
+export const userChatStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "0.3em 0.4em",
+    paddingRight: "1em"
+  },
+  avatarStyle: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+    background: "orange",
+    boxShadow: theme.shadows[2]
+  },
+  label: {
+    fontSize: "0.8em",
+    paddingTop: "0.2em"
+  },
+  userDetail: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  userName: {
+    margin: "0em 1em",
+    display: "flex",
+    flexDirection: "column"
+  },
+  setting: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}));
